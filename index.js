778094bb1e26b0217fa7ffa58b267584abe84cdd
ZombGuy <zombguy1234@gmail.com>
2022-09-02 01:06:56 -0500

    Working prototype chat

---
+++ index.js
@@ -0,0 +1,38 @@
+//Server npm package
+// Include the server in your file
+// const server = require('server');
+// const { get, post } = server.router;
+
+// // Handle requests to the url "/" ( http://localhost:3000/ )
+// server([
+//   get('/', ctx => 'Hello world!')
+// ]);
+
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
+
+  io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg)
+        io.emit('chat message', msg);
+    });
+  });
+
+server.listen(20860, () => {
+  console.log('listening on *:20860');
+});
