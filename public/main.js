567d53f8ddbdbf7857c6d0412e8c1d94ad1415ec
ZombGuy <zombguy1234@gmail.com>
2022-09-02 02:28:49 -0500

    Default nickname and some content layout fixes

--- public/main.js
+++ public/main.js
@@ -1,13 +1,34 @@
 const para = document.getElementById("nickname");
         para.addEventListener('click', updateName);
         let debug = true;
+        
+        
+
+        function nameCheck() {
+            let name = localStorage.getItem('session-nick');
+            if(name != (null || "null")){
+                console.log("Loading nickname!");
+                para.textContent = "nickname: " + name;
+            }else{
+                updateName();
+            }
+        }
+
         function updateName() {
             let name = prompt('Enter the user name');
-            if(debug == true){
-                console.log("User prompted for name change.");
-                console.log("User changed name to " + name + ".");
+            switch(name){
+                case null:
+
+                case "null":
+                    name = "Guest";
+                default:
+                    if(debug == true){
+                        console.log("User prompted for name change.");
+                        console.log("User changed name to " + name + ".");
+                    }
+                    localStorage.setItem( 'session-nick', name);
+                    para.textContent = "nickname: " + name
             }
-            sessionStorage.setItem( 'session-nick', name);
-            para.textContent = "nickname: " + name
+            
             
         }
